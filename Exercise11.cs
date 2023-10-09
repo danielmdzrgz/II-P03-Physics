9b2e8924289bcafd9f3743d82986fe5cb36c14c1
Daniel Méndez <72448758+danielmdzrgz@users.noreply.github.com>
2023-10-09 18:20:52 +0100

    Files of exercise11

---
+++ Exercise11.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise11 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     string collidedObjectTag = collision.gameObject.tag;
+
+    //     Debug.Log("Collided with " + collidedObjectTag);
+    // }
+
+    void OnTriggerEnter(Collider other)
+    {
+        string collidedObjectTag = other.gameObject.tag;
+
+        Debug.Log("Collided with " + collidedObjectTag);
+    }
+}
