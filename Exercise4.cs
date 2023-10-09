4757f02fd145449ab69133c8d9ee2df38d36d5c0
Daniel Méndez <72448758+danielmdzrgz@users.noreply.github.com>
2023-10-09 18:17:23 +0100

    Files of exercise4

---
+++ Exercise4.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise4 : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || 
+            Input.GetKeyDown(KeyCode.DownArrow) || 
+            Input.GetKeyDown(KeyCode.LeftArrow) || 
+            Input.GetKeyDown(KeyCode.RightArrow)) && 
+            gameObject.name == "MyCube") {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 cubeMovement = new Vector3(horizontalInput, 0.0f, verticalInput) * speed;
+            transform.Translate(cubeMovement); 
+        }
+        
+        if ((Input.GetKeyDown(KeyCode.W) || 
+            Input.GetKeyDown(KeyCode.S) || 
+            Input.GetKeyDown(KeyCode.A) || 
+            Input.GetKeyDown(KeyCode.D)) && 
+            gameObject.name == "MySphere") {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 sphereMovement = new Vector3(horizontalInput, 0.0f, verticalInput) * speed;
+            transform.Translate(sphereMovement); 
+        }
+    }
+}
