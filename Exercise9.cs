47bde96710d5bba609703d8367c1149f84211872
Daniel Méndez <72448758+danielmdzrgz@users.noreply.github.com>
2023-10-09 18:19:49 +0100

    Files of exercise9

---
+++ Exercise9.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise9 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string collidedObjectTag = collision.gameObject.tag;
+
+        Debug.Log("Collided with " + collidedObjectTag);
+    }
+}
